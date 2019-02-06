cbf930fc12adedb3700d778635d709974fed4a7c
Milica <milicarodic307@yahoo.com>
2019-02-06 09:04:12 -0600

    starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="logo-box">
+            <img src="logo-green.png" className="logo" alt="logo" />
+          </div>
+          <div className="text-box">
+            <h1 className="heading-primary">
+              <span className="heading-primary-main">
+                Welcome to Clear Cut Analytics
+              </span>
+              <span className="heading-primary-sub">
+                In particular, the garbled words of lorem ipsum bear an
+                unmistakable resemblance
+              </span>
+              <span className="heading-primary-sub">
+                to sections 1.10.32–33 of Cicero's work, with the most notable
+                passage excerpted below:
+              </span>
+            </h1>
+
+            <button className="btnAction">Click me</button>
+          </div>
         </header>
       </div>
     );
